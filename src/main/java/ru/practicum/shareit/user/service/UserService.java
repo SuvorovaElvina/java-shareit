f812277ee2b3c6029b2a183cccb2e73cc4e608cf
Elvina <elvina.smirnova.205@mail.ru>
2023-05-02 21:03:43 +0300

    feat: создан сервис с бизнес-логикой

---
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -0,0 +1,18 @@
+package ru.practicum.shareit.user.service;
+
+import ru.practicum.shareit.user.User;
+import ru.practicum.shareit.user.dto.UserDto;
+
+import java.util.List;
+
+public interface UserService {
+    User add(UserDto user);
+
+    User update(long id, UserDto user);
+
+    User getById(long id);
+
+    List<User> getAll();
+
+    void delete(long id);
+}
---
+++ src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
@@ -0,0 +1,87 @@
+package ru.practicum.shareit.user.service;
+
+import org.springframework.stereotype.Service;
+import ru.practicum.shareit.exception.DuplicateException;
+import ru.practicum.shareit.exception.IncorrectCountException;
+import ru.practicum.shareit.exception.NotFoundException;
+import ru.practicum.shareit.user.User;
+import ru.practicum.shareit.user.UserMapper;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.repositiry.UserRepository;
+import ru.practicum.shareit.user.repositiry.UserRepositoryImpl;
+import ru.practicum.shareit.user.service.UserService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository repository = new UserRepositoryImpl();
+    private final UserMapper mapper = new UserMapper();
+
+    @Override
+    public User add(UserDto userDto) {
+        User user = mapper.toUser(userDto);
+        return repository.add(user);
+    }
+
+    @Override
+    public User update(long id, UserDto userDto) {
+        User user = getById(id);
+        name(user, userDto);
+        email(user, userDto);
+        return repository.update(user);
+    }
+
+    private void name(User user, UserDto userDto) {
+        try {
+            if (!userDto.getName().isEmpty()) {
+                user.setName(userDto.getName());
+            }
+        } catch (NullPointerException e) {
+        }
+    }
+
+    private void email(User user, UserDto userDto) {
+        try {
+            if (!userDto.getEmail().isBlank()) {
+                for (User user1 : repository.getAll().values()) {
+                    if (user1.getEmail().equals(userDto.getEmail()) & (!Objects.equals(user.getId(), user1.getId()))) {
+                        throw new DuplicateException("Эта почта уже используется, введите другую.");
+                    }
+                }
+                user.setEmail(userDto.getEmail());
+            }
+        } catch (NullPointerException e) {
+        }
+    }
+
+    @Override
+    public User getById(long id) {
+        validateId(id);
+        return repository.getAll().get(id);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return new ArrayList<>(repository.getAll().values());
+    }
+
+    @Override
+    public void delete(long id) {
+        validateId(id);
+        repository.delete(id);
+    }
+
+    private void validateId(long id) {
+        if (id < 0) {
+            throw new IncorrectCountException("id не должно быть меньше 0.");
+        }
+        try {
+            repository.getAll().get(id);
+        } catch (NullPointerException e) {
+            throw new NotFoundException(String.format("Пользователь с id %d - не существует.", id));
+        }
+    }
+}
