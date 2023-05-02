f33578bb1d563abfcbae51e71821c56a1535aa7a
Elvina <elvina.smirnova.205@mail.ru>
2023-05-02 21:07:11 +0300

    feat: создана ошибка "не корректное id"

---
+++ src/main/java/ru/practicum/shareit/exception/IncorrectCountException.java
@@ -0,0 +1,7 @@
+package ru.practicum.shareit.exception;
+
+public class IncorrectCountException extends RuntimeException {
+    public IncorrectCountException(String message) {
+        super(message);
+    }
+}
