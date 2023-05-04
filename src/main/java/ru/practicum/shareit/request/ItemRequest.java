a89fc9b586eb46475f9490c8aef8c4249f858900
Elvina <elvina.smirnova.205@mail.ru>
2023-05-04 19:42:50 +0300

    feat: заготовка функциональности

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -1,7 +1,19 @@
 package ru.practicum.shareit.request;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ItemRequest {
+    long id;
+    User user;
+    String description;
+    LocalDate created;
 }
