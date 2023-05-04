569eaf4d44d2745617b42f2a63688f3e714547d0
Elvina <elvina.smirnova.205@mail.ru>
2023-05-04 19:40:26 +0300

    feat: добавила валидацию

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,25 @@
 package ru.practicum.shareit.item.dto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+import ru.practicum.shareit.user.model.User;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ItemDto {
+    Long id;
+    User owner;
+    @NotBlank(message = "Имя не должно быть пустым.")
+    String name;
+    @NotBlank(message = "Описание не должно быть пустым.")
+    String description;
+    @NotNull
+    Boolean available;
+    Long request;
 }
--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -5,11 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @Data
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class User {
     Long id;
+    @NotBlank
     String name;
+    @Email
+    @NotBlank
     String email;
 }
