fdcbb69716a80ef85fa10900af3d7bd8d01f895b
Elvina <elvina.smirnova.205@mail.ru>
2023-05-17 19:03:27 +0300

    feat: использование hibernate

--- src/main/java/ru/practicum/shareit/booking/model/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -1,22 +1,33 @@
-package ru.practicum.shareit.booking;
+package ru.practicum.shareit.booking.model;
 
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
-import ru.practicum.shareit.booking.status.Status;
+import ru.practicum.shareit.booking.enums.Status;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "bookings")
 @Data
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    @Column(name = "start_date")
     LocalDate start;
+    @Column(name = "end_date")
     LocalDate end;
+    @OneToOne(fetch = FetchType.LAZY)
     User booker;
+    @OneToOne(fetch = FetchType.LAZY)
     Item item;
+    @Enumerated(EnumType.STRING)
     Status status;
 }
