65c0644e573e95cbe8b13c49cce0ec84c65e4d5a
Elvina <elvina.smirnova.205@mail.ru>
2023-06-08 13:13:03 +0300

    fix: убрала лишнюю аннатацию

--- src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.status.Status;
 
@@ -13,7 +12,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
-@Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     Optional<List<Booking>> findByItemIdAndBookerIdAndEndBeforeAndStatusNotLike(Long itemId, Long bookerId, LocalDateTime time, Status status);
 
--- src/main/java/ru/practicum/shareit/request/reposiroty/ItemRequestRepository.java
+++ src/main/java/ru/practicum/shareit/request/reposiroty/ItemRequestRepository.java
@@ -3,10 +3,8 @@ package ru.practicum.shareit.request.reposiroty;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.request.model.ItemRequest;
 
-@Repository
 public interface ItemRequestRepository extends JpaRepository<ItemRequest, Long> {
     Page<ItemRequest> findByOwnerId(Long userId, Pageable pageable);
 
