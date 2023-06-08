6653023377e1e1001468d70ad8af222cbabf7257
Elvina <elvina.smirnova.205@mail.ru>
2023-06-08 19:35:02 +0300

    style: убрала неиспользуемые импорты

--- src/main/java/ru/practicum/shareit/item/comment/repository/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/comment/repository/CommentRepository.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.item.comment.repository;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,7 +8,6 @@ import ru.practicum.shareit.item.model.Item;
 
 import javax.transaction.Transactional;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.model.ItemRequest;
 
 import java.util.List;
