67c10fba2aed9da73fc06d23c0e1e5e8cbb0c828
Elvina <elvina.smirnova.205@mail.ru>
2023-06-08 19:34:44 +0300

    test: добавила тесты

---
+++ src/test/java/ru/practicum/shareit/item/ItemTest.java
@@ -0,0 +1,22 @@
+package ru.practicum.shareit.item;
+
+import org.junit.jupiter.api.Test;
+import ru.practicum.shareit.item.model.Item;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemTest {
+    Item item = Item.builder().id(1L).build();
+
+    @Test
+    void testEquals() {
+        assertTrue(item.equals(Item.builder().id(1L).build()));
+    }
+
+    @Test
+    void testHashCode() {
+        Item item1 = Item.builder().id(1L).build();
+
+        assertEquals(item1.hashCode(), item.hashCode());
+    }
+}
